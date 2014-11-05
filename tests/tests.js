8d3a179d3fe99f384082bff31cc96e2f6dcb1f5c
Gabriel Littman <gabe@dispatchr.co>
2014-11-05 14:56:26 -0800

    updates to use tape testing framework

--- tests/tests.js
+++ tests/tests.js
@@ -1,58 +1,51 @@
-function assert(x) {
-    if (!x) {
-        console.error("Assert failed");
-        throw new Exception("Assert failed");
-        // debugger;
-    }
-}
 
+var tape = require('tape');
 var vComp = require('../version_compare');
 
-assert(vComp.compare("1.7.1", "1.7.10") < 0);
-assert(vComp.compare("1.7.2", "1.7.10") < 0);
-assert(vComp.compare("1.6.1", "1.7.10") < 0);
-assert(vComp.compare("1.6.20", "1.7.10") < 0);
-assert(vComp.compare("1.7.1", "1.7.10") < 0);
-assert(vComp.compare("1.7", "1.7.0") < 0);
-assert(vComp.compare("1.7", "1.8.0") < 0);
+tape(function(test){
+  test.true(vComp.compare("1.7.1", "1.7.10") < 0);
+  test.true(vComp.compare("1.7.2", "1.7.10") < 0);
+  test.true(vComp.compare("1.6.1", "1.7.10") < 0);
+  test.true(vComp.compare("1.6.20", "1.7.10") < 0);
+  test.true(vComp.compare("1.7.1", "1.7.10") < 0);
+  test.true(vComp.compare("1.7", "1.7.0") < 0);
+  test.true(vComp.compare("1.7", "1.8.0") < 0);
 
-assert(vComp.compare("1.7.2", "1.7.10b", {lexicographical: true}) > 0);
-assert(vComp.compare("1.7.10", "1.7.1") > 0);
-assert(vComp.compare("1.7.10", "1.6.1") > 0);
-assert(vComp.compare("1.7.10", "1.6.20") > 0);
-assert(vComp.compare("1.7.0", "1.7") > 0);
-assert(vComp.compare("1.8.0", "1.7") > 0);
+  test.true(vComp.compare("1.7.2", "1.7.10b", {lexicographical: true}) > 0);
+  test.true(vComp.compare("1.7.10", "1.7.1") > 0);
+  test.true(vComp.compare("1.7.10", "1.6.1") > 0);
+  test.true(vComp.compare("1.7.10", "1.6.20") > 0);
+  test.true(vComp.compare("1.7.0", "1.7") > 0);
+  test.true(vComp.compare("1.8.0", "1.7") > 0);
 
-assert(vComp.compare("1.7.10", "1.7.10") === 0);
-assert(vComp.compare("1.7", "1.7") === 0);
-assert(vComp.compare("1.7", "1.7.0", {zeroExtend: true}) === 0);
+  test.true(vComp.compare("1.7.10", "1.7.10") === 0);
+  test.true(vComp.compare("1.7", "1.7") === 0);
+  test.true(vComp.compare("1.7", "1.7.0", {zeroExtend: true}) === 0);
 
-assert(isNaN(vComp.compare("1.7", "1..7")));
-assert(isNaN(vComp.compare("1.7", "Bad")));
-assert(isNaN(vComp.compare("1..7", "1.7")));
-assert(isNaN(vComp.compare("Bad", "1.7")));
+  test.true(isNaN(vComp.compare("1.7", "1..7")));
+  test.true(isNaN(vComp.compare("1.7", "Bad")));
+  test.true(isNaN(vComp.compare("1..7", "1.7")));
+  test.true(isNaN(vComp.compare("Bad", "1.7")));
 
-assert(!vComp.matches("1.6", "1.7"));
-assert( vComp.matches("1.7", "1.7"));
-assert(!vComp.matches("1.8", "1.7"));
+  test.false(vComp.matches("1.6", "1.7"));
+  test.true( vComp.matches("1.7", "1.7"));
+  test.false(vComp.matches("1.8", "1.7"));
 
-assert(!vComp.gt("1.6", "1.7"));
-assert(!vComp.gt("1.7", "1.7"));
-assert( vComp.gt("1.8", "1.7"));
+  test.false(vComp.gt("1.6", "1.7"));
+  test.false(vComp.gt("1.7", "1.7"));
+  test.true( vComp.gt("1.8", "1.7"));
 
-assert(!vComp.gte("1.6", "1.7"));
-assert( vComp.gte("1.7", "1.7"));
-assert( vComp.gte("1.8", "1.7"));
+  test.false(vComp.gte("1.6", "1.7"));
+  test.true( vComp.gte("1.7", "1.7"));
+  test.true( vComp.gte("1.8", "1.7"));
 
-assert( vComp.lt("1.6", "1.7"));
-assert(!vComp.lt("1.7", "1.7"));
-assert(!vComp.lt("1.8", "1.7"));
+  test.true( vComp.lt("1.6", "1.7"));
+  test.false(vComp.lt("1.7", "1.7"));
+  test.false(vComp.lt("1.8", "1.7"));
 
-assert( vComp.lte("1.6", "1.7"));
-assert( vComp.lte("1.7", "1.7"));
-assert(!vComp.lte("1.8", "1.7"));
-
-
-
-
-console.error("All done");
+  test.true( vComp.lte("1.6", "1.7"));
+  test.true( vComp.lte("1.7", "1.7"));
+  test.false(vComp.lte("1.8", "1.7"));
+  
+  test.end();
+});
