0de486792787f207b229fab2668fd84d41849134
Gabriel Littman <gabe@dispatchr.co>
2014-11-05 14:23:59 -0800

    adding some convenience methods to module

--- tests/tests.js
+++ tests/tests.js
@@ -6,30 +6,53 @@ function assert(x) {
     }
 }
 
-var versionCompare = require('../version_compare');
+var vComp = require('../version_compare');
 
-assert(versionCompare("1.7.1", "1.7.10") < 0);
-assert(versionCompare("1.7.2", "1.7.10") < 0);
-assert(versionCompare("1.6.1", "1.7.10") < 0);
-assert(versionCompare("1.6.20", "1.7.10") < 0);
-assert(versionCompare("1.7.1", "1.7.10") < 0);
-assert(versionCompare("1.7", "1.7.0") < 0);
-assert(versionCompare("1.7", "1.8.0") < 0);
+assert(vComp.compare("1.7.1", "1.7.10") < 0);
+assert(vComp.compare("1.7.2", "1.7.10") < 0);
+assert(vComp.compare("1.6.1", "1.7.10") < 0);
+assert(vComp.compare("1.6.20", "1.7.10") < 0);
+assert(vComp.compare("1.7.1", "1.7.10") < 0);
+assert(vComp.compare("1.7", "1.7.0") < 0);
+assert(vComp.compare("1.7", "1.8.0") < 0);
 
-assert(versionCompare("1.7.2", "1.7.10b", {lexicographical: true}) > 0);
-assert(versionCompare("1.7.10", "1.7.1") > 0);
-assert(versionCompare("1.7.10", "1.6.1") > 0);
-assert(versionCompare("1.7.10", "1.6.20") > 0);
-assert(versionCompare("1.7.0", "1.7") > 0);
-assert(versionCompare("1.8.0", "1.7") > 0);
+assert(vComp.compare("1.7.2", "1.7.10b", {lexicographical: true}) > 0);
+assert(vComp.compare("1.7.10", "1.7.1") > 0);
+assert(vComp.compare("1.7.10", "1.6.1") > 0);
+assert(vComp.compare("1.7.10", "1.6.20") > 0);
+assert(vComp.compare("1.7.0", "1.7") > 0);
+assert(vComp.compare("1.8.0", "1.7") > 0);
 
-assert(versionCompare("1.7.10", "1.7.10") === 0);
-assert(versionCompare("1.7", "1.7") === 0);
-assert(versionCompare("1.7", "1.7.0", {zeroExtend: true}) === 0);
+assert(vComp.compare("1.7.10", "1.7.10") === 0);
+assert(vComp.compare("1.7", "1.7") === 0);
+assert(vComp.compare("1.7", "1.7.0", {zeroExtend: true}) === 0);
 
-assert(isNaN(versionCompare("1.7", "1..7")));
-assert(isNaN(versionCompare("1.7", "Bad")));
-assert(isNaN(versionCompare("1..7", "1.7")));
-assert(isNaN(versionCompare("Bad", "1.7")));
+assert(isNaN(vComp.compare("1.7", "1..7")));
+assert(isNaN(vComp.compare("1.7", "Bad")));
+assert(isNaN(vComp.compare("1..7", "1.7")));
+assert(isNaN(vComp.compare("Bad", "1.7")));
+
+assert(!vComp.matches("1.6", "1.7"));
+assert( vComp.matches("1.7", "1.7"));
+assert(!vComp.matches("1.8", "1.7"));
+
+assert(!vComp.gt("1.6", "1.7"));
+assert(!vComp.gt("1.7", "1.7"));
+assert( vComp.gt("1.8", "1.7"));
+
+assert(!vComp.gte("1.6", "1.7"));
+assert( vComp.gte("1.7", "1.7"));
+assert( vComp.gte("1.8", "1.7"));
+
+assert( vComp.lt("1.6", "1.7"));
+assert(!vComp.lt("1.7", "1.7"));
+assert(!vComp.lt("1.8", "1.7"));
+
+assert( vComp.lte("1.6", "1.7"));
+assert( vComp.lte("1.7", "1.7"));
+assert(!vComp.lte("1.8", "1.7"));
+
+
+
 
 console.error("All done");
