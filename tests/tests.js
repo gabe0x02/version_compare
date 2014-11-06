43368b5f02a77c2e62940155e62bcd1659f8e8f1
Gabriel Littman <gabe@dispatchr.co>
2014-11-05 16:36:32 -0800

    moved tests to mocha

--- tests/tests.js
+++ tests/tests.js
@@ -1,51 +1,52 @@
-
-var tape = require('tape');
+var should = require('should');
 var vComp = require('../version_compare');
 
-tape(function(test){
-  test.true(vComp.compare("1.7.1", "1.7.10") < 0);
-  test.true(vComp.compare("1.7.2", "1.7.10") < 0);
-  test.true(vComp.compare("1.6.1", "1.7.10") < 0);
-  test.true(vComp.compare("1.6.20", "1.7.10") < 0);
-  test.true(vComp.compare("1.7.1", "1.7.10") < 0);
-  test.true(vComp.compare("1.7", "1.7.0") < 0);
-  test.true(vComp.compare("1.7", "1.8.0") < 0);
-
-  test.true(vComp.compare("1.7.2", "1.7.10b", {lexicographical: true}) > 0);
-  test.true(vComp.compare("1.7.10", "1.7.1") > 0);
-  test.true(vComp.compare("1.7.10", "1.6.1") > 0);
-  test.true(vComp.compare("1.7.10", "1.6.20") > 0);
-  test.true(vComp.compare("1.7.0", "1.7") > 0);
-  test.true(vComp.compare("1.8.0", "1.7") > 0);
-
-  test.true(vComp.compare("1.7.10", "1.7.10") === 0);
-  test.true(vComp.compare("1.7", "1.7") === 0);
-  test.true(vComp.compare("1.7", "1.7.0", {zeroExtend: true}) === 0);
-
-  test.true(isNaN(vComp.compare("1.7", "1..7")));
-  test.true(isNaN(vComp.compare("1.7", "Bad")));
-  test.true(isNaN(vComp.compare("1..7", "1.7")));
-  test.true(isNaN(vComp.compare("Bad", "1.7")));
-
-  test.false(vComp.matches("1.6", "1.7"));
-  test.true( vComp.matches("1.7", "1.7"));
-  test.false(vComp.matches("1.8", "1.7"));
-
-  test.false(vComp.gt("1.6", "1.7"));
-  test.false(vComp.gt("1.7", "1.7"));
-  test.true( vComp.gt("1.8", "1.7"));
-
-  test.false(vComp.gte("1.6", "1.7"));
-  test.true( vComp.gte("1.7", "1.7"));
-  test.true( vComp.gte("1.8", "1.7"));
-
-  test.true( vComp.lt("1.6", "1.7"));
-  test.false(vComp.lt("1.7", "1.7"));
-  test.false(vComp.lt("1.8", "1.7"));
-
-  test.true( vComp.lte("1.6", "1.7"));
-  test.true( vComp.lte("1.7", "1.7"));
-  test.false(vComp.lte("1.8", "1.7"));
+describe('My module', function() {
+  it('handles compares', function() {
   
-  test.end();
+    vComp.compare("1.7.1", "1.7.10").should.be.lessThan(0);
+    vComp.compare("1.7.2", "1.7.10").should.be.lessThan(0);
+    vComp.compare("1.6.1", "1.7.10").should.be.lessThan(0);
+    vComp.compare("1.6.20", "1.7.10").should.be.lessThan(0);
+    vComp.compare("1.7.1", "1.7.10").should.be.lessThan(0);
+    vComp.compare("1.7", "1.7.0").should.be.lessThan(0);
+    vComp.compare("1.7", "1.8.0").should.be.lessThan(0);
+
+    vComp.compare("1.7.2", "1.7.10b", {lexicographical: true}).should.be.greaterThan(0);
+    vComp.compare("1.7.10", "1.7.1").should.be.greaterThan(0);
+    vComp.compare("1.7.10", "1.6.1").should.be.greaterThan(0);
+    vComp.compare("1.7.10", "1.6.20").should.be.greaterThan(0);
+    vComp.compare("1.7.0", "1.7").should.be.greaterThan(0);
+    vComp.compare("1.8.0", "1.7").should.be.greaterThan(0);
+
+    vComp.compare("1.7.10", "1.7.10").should.equal(0);
+    vComp.compare("1.7", "1.7").should.equal(0);
+    vComp.compare("1.7", "1.7.0", {zeroExtend: true}).should.equal(0);
+
+    vComp.compare("1.7", "1..7").should.be.NaN;
+    vComp.compare("1.7", "Bad").should.be.NaN;
+    vComp.compare("1..7", "1.7").should.be.NaN;
+    vComp.compare("Bad", "1.7").should.be.NaN;
+
+    vComp.matches("1.6", "1.7").should.be.false;
+    vComp.matches("1.7", "1.7").should.be.true;
+    vComp.matches("1.8", "1.7").should.be.false;
+
+    vComp.gt("1.6", "1.7").should.be.false;
+    vComp.gt("1.7", "1.7").should.be.false;
+    vComp.gt("1.8", "1.7").should.be.true;
+
+    vComp.gte("1.6", "1.7").should.be.false;
+    vComp.gte("1.7", "1.7").should.be.true;
+    vComp.gte("1.8", "1.7").should.be.true;
+
+    vComp.lt("1.6", "1.7").should.be.true;
+    vComp.lt("1.7", "1.7").should.be.false;
+    vComp.lt("1.8", "1.7").should.be.false;
+
+    vComp.lte("1.6", "1.7").should.be.true;
+    vComp.lte("1.7", "1.7").should.be.true;
+    vComp.lte("1.8", "1.7").should.be.false;
+  
+  });
 });
